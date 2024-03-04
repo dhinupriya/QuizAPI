7434bb7bcb35472f2104136455d02bcb205cda07
dhinupriya <dhinupriyacs@gmail.com>
2024-03-03 17:47:45 -0700

    delete all files

--- src/main/java/com/RegisterQuizApplication/Registration/ScoreRepository.java
+++
@@ -1,6 +0,0 @@
-package com.RegisterQuizApplication.Registration;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScoreRepository extends JpaRepository<Score,Integer> {
-}
